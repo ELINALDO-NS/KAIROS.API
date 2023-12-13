4b9f3aa349b8b1284be0e4d1f90cda688c4d3055
ELINALDO-NS <elinaldo_nascimento@outlook.com>
2023-12-13 11:46:53 -0300

    Criação do Layout do FORMS

---
+++ KAIROS.API/KAIROS.API/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace KAIROS.API
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            InsereCadastros = new TabPage();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            textBox6 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
+            InsereCadastros.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(InsereCadastros);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(1, -2);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(530, 308);
+            tabControl1.TabIndex = 0;
+            // 
+            // InsereCadastros
+            // 
+            InsereCadastros.Controls.Add(button4);
+            InsereCadastros.Controls.Add(button3);
+            InsereCadastros.Controls.Add(button2);
+            InsereCadastros.Controls.Add(textBox6);
+            InsereCadastros.Controls.Add(label6);
+            InsereCadastros.Controls.Add(textBox5);
+            InsereCadastros.Controls.Add(label5);
+            InsereCadastros.Controls.Add(textBox4);
+            InsereCadastros.Controls.Add(label4);
+            InsereCadastros.Controls.Add(textBox3);
+            InsereCadastros.Controls.Add(label3);
+            InsereCadastros.Controls.Add(textBox2);
+            InsereCadastros.Controls.Add(label2);
+            InsereCadastros.Controls.Add(button1);
+            InsereCadastros.Controls.Add(textBox1);
+            InsereCadastros.Controls.Add(label1);
+            InsereCadastros.Location = new Point(4, 24);
+            InsereCadastros.Name = "InsereCadastros";
+            InsereCadastros.Padding = new Padding(3);
+            InsereCadastros.Size = new Size(522, 280);
+            InsereCadastros.TabIndex = 0;
+            InsereCadastros.Text = "Insere Cadastros";
+            InsereCadastros.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button4.Location = new Point(268, 177);
+            button4.Name = "button4";
+            button4.Size = new Size(144, 23);
+            button4.TabIndex = 15;
+            button4.Text = "Iniciar";
+            button4.TextAlign = ContentAlignment.TopCenter;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button3.Location = new Point(268, 131);
+            button3.Name = "button3";
+            button3.Size = new Size(144, 23);
+            button3.TabIndex = 14;
+            button3.Text = "Valida Dados";
+            button3.TextAlign = ContentAlignment.TopCenter;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button2.Location = new Point(268, 93);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 23);
+            button2.TabIndex = 13;
+            button2.Text = "Lista Horarios";
+            button2.TextAlign = ContentAlignment.TopCenter;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            textBox6.Location = new Point(123, 177);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(130, 25);
+            textBox6.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label6.Location = new Point(7, 185);
+            label6.Name = "label6";
+            label6.Size = new Size(111, 17);
+            label6.TabIndex = 11;
+            label6.Text = "CPF Responsavel";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            textBox5.Location = new Point(62, 131);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(193, 25);
+            textBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label5.Location = new Point(7, 139);
+            label5.Name = "label5";
+            label5.Size = new Size(39, 17);
+            label5.TabIndex = 9;
+            label5.Text = "CNPJ";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            textBox4.Location = new Point(62, 93);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(193, 25);
+            textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label4.Location = new Point(7, 101);
+            label4.Name = "label4";
+            label4.Size = new Size(28, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Key";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            textBox3.Location = new Point(62, 55);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(193, 25);
+            textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label3.Location = new Point(7, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 17);
+            label3.TabIndex = 5;
+            label3.Text = "Usuario";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            textBox2.Location = new Point(315, 55);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(97, 25);
+            textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label2.Location = new Point(268, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 17);
+            label2.TabIndex = 3;
+            label2.Text = "Senha";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button1.Location = new Point(322, 10);
+            button1.Name = "button1";
+            button1.Size = new Size(58, 23);
+            button1.TabIndex = 2;
+            button1.Text = "...";
+            button1.TextAlign = ContentAlignment.TopCenter;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            textBox1.Location = new Point(113, 10);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(193, 25);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label1.Location = new Point(7, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 17);
+            label1.TabIndex = 0;
+            label1.Text = "Caminho Excel";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(522, 280);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Insere Saldo BH";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(530, 307);
+            Controls.Add(tabControl1);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "KAIROS.API";
+            tabControl1.ResumeLayout(false);
+            InsereCadastros.ResumeLayout(false);
+            InsereCadastros.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage InsereCadastros;
+        private TabPage tabPage2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox4;
+        private Label label4;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private TextBox textBox6;
+        private Label label6;
+        private TextBox textBox5;
+        private Label label5;
+    }
+}

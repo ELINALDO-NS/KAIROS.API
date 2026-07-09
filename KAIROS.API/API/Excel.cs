732443491e31df532fa3976c3944b52a4d10f90d
Elinaldo Nascimento <elinaldo_nascimento@outlook.com>
2026-07-09 14:05:11 -0300

    Feat: Adicionando validação de colunas planilha de comunicação

--- KAIROS.API/API/Excel.cs
+++ KAIROS.API/API/Excel.cs
@@ -18,17 +18,36 @@ namespace API
 
         public List<string> ValidaColunasExcel(string Planilha, bool Comunicacao)
         {
-            if (Comunicacao)
-            {
-                return [];
-            }
-
             ExcelWorksheet PlanilaSelecionada = Planila.Workbook.Worksheets.First(a => a.Name == Planilha);
             if (PlanilaSelecionada == null)
             {
                 throw new Exception("Planilha não encontrada !");
             }
             string DadoLido = string.Empty;
+            if (Comunicacao)
+            {
+                List<string> colunasEsperadasComunica = new()
+                {
+                    "matricula","nome","pis","crachá",
+                    "data de admissão","data de nascimento","cpf",
+                    "e-mail","departamento","sexo","cnpj"
+                };
+                List<string> colunasEncontradasComunica = new();
+                List<string> colunasNaoEncontradasComunica = new();
+
+                for (int i = 1; i < 25; i++)
+                {
+                    DadoLido = Convert.ToString(PlanilaSelecionada.Cells[3, i].Value).ToLower() ?? string.Empty;
+                    if (!string.IsNullOrEmpty(DadoLido))
+                    {
+                        colunasEncontradasComunica.Add(DadoLido.Trim());
+                    }
+                }
+
+                return colunasEsperadasComunica.Except(colunasEncontradasComunica).ToList();
+            }
+
+
             List<string> colunasEsperadas = new()
             {
                 "matricula","nome","pis","crachá","data de nascimento",
